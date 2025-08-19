65a751643206accdcd418b809bc48966af643cb2
sameerm38 <sameer.m38@gmail.com>
2025-08-19 09:20:07 +0530

    CQRS outline

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,22 @@
+﻿
+
+using BuildingBlocks.CQRS;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, 
+        List<string> Category,
+        string Description,
+        string ImageFile,
+        decimal Price
+        ):ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            //Business logic to create a product
+            throw new NotImplementedException();
+        }
+    }
+}
