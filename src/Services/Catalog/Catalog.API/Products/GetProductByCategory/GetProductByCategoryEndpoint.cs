4b5075e8e697637d98bfb844dd6ba0a50c6061b7
sameerm38 <sameer.m38@gmail.com>
2025-08-23 23:34:37 +0530

    endpoints

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,30 @@
+﻿
+using Catalog.API.Products.GetProductById;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Products.GetProductByCategory
+{
+    //public record GetProductByCategoryRequest();
+    public record GetProductByCategoryResponse(IEnumerable<Product> Products);
+    public class GetProductByCategoryEndpoint : ICarterModule
+    {
+        
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+           app.MapGet("/Products/category/{Category}",
+               async (string Category,ISender sender)=>{
+                   var result = await sender.Send(new GetProductByCategoryQuery(Category));
+                   var response = result.Adapt<GetProductByCategoryResponse>();
+                   return Results.Ok(response);
+            })
+                .WithName("GetProductByCategory")
+                .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Get Product By Category")
+                .WithDescription("Get Product By Category");
+        }
+
+       
+       
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,25 @@
+﻿namespace Catalog.API.Products.GetProductById
+{
+    //public record GetProductByIdRequest(Guid Id);
+    public record GetProductByIdResponse(Product Product);
+    public class GetProductByIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/Products/{id}", 
+                async (Guid Id,ISender sender) =>
+            {
+               // var query = request.Adapt<GetProductByIdQuery>();
+                var responce = await sender.Send(new GetProductByIdQuery(Id));
+                var result = responce.Adapt<GetProductByIdResponse>();
+                return Results.Ok(result);
+
+            })
+                .WithName("GetProductById")
+                .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Get Product By Id")
+                .WithDescription("Get Product By Id");
+        }
+    }
+}
